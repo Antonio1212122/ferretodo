2a803fab679755c7ccc9c355b5e1d3f5658043f4
mario <maripadilla2f@gmail.com>
2025-03-27 17:55:03 -0600

    detalles esteticos del boton

--- resources/views/catalogos/proveedoresAgregarGet.blade.php
+++ resources/views/catalogos/proveedoresAgregarGet.blade.php
@@ -4,14 +4,27 @@
     @component("components.breadcrumbs",["breadcrumbs"=>$breadcrumbs])
     @endcomponent
 
-    <h1>Agregar Proveedor</h1>
+    <div class="row">
+        <div class="form-group my-3">
+            <h1>Agregar Proveedor</h1>
+        </div>
+    </div>
 
-    <form action="{{ url('/catalogos/proveedores/agregar') }}" method="POST">
-        @csrf  {{-- NECESARIO para evitar errores 419 --}}
+    <form method="post" action="{{ url('/catalogos/proveedores/agregar') }}">
+        @csrf {{-- NECESARIO para evitar errores 419 --}}
+        
+        <div class="row my-4">
+            <div class="form-group mb-3 col-6">
+                <label for="nombre">Nombre:</label>
+                <input type="text" maxlength="50" class="form-control" name="nombre" placeholder="Ingrese el nombre del proveedor" id="nombre" required autofocus>
+            </div>
+        </div>
 
-        <label>Nombre:</label>
-        <input type="text" name="nombre" required>
-
-        <button type="submit">Guardar</button>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary">Guardar</button>
+            </div>
+        </div>
     </form>
 @endsection
