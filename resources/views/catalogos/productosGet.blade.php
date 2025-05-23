5106398cf8d0aa883001c62ddd854e8926e379d2
Antonio1212122 <jantonioguerra055@gmail.com>
2025-05-23 13:58:49 -0600

    idiomas 2.0

--- resources/views/catalogos/productosGet.blade.php
+++ resources/views/catalogos/productosGet.blade.php
@@ -42,7 +42,13 @@
 </table>
 
 <script>
-    let table = new DataTable('#maintable', { paging: true, searching: true });
+    let table = new DataTable('#maintable', {
+        paging: true,
+        searching: true,
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
+        }
+    });
 </script>
 
 @endsection
--- resources/views/catalogos/proveedoresGet.blade.php
+++ resources/views/catalogos/proveedoresGet.blade.php
@@ -31,7 +31,13 @@
 </table>
 
 <script>
-    let table = new DataTable('#maintable', { paging: true, searching: true });
+    let table = new DataTable('#maintable', {
+        paging: true,
+        searching: true,
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
+        }
+    });
 </script>
 
 @endsection
