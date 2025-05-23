a0ac6cab65e51f044917c0a4862e874b505e38d2
Antonio1212122 <jantonioguerra055@gmail.com>
2025-05-23 14:08:30 -0600

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-6 offset-md-3">
             <div class="card">
                 <div class="card-body">
-                    <form action="{{ url('/login') }}" method="POST">
+                    <form action="{{ route('/login') }}" method="POST">
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Correo Electrónico</label>
